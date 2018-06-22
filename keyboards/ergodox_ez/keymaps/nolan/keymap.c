96ca3a65fa253f001f4308c3da97176cd6083d2f
nolan.seaton <nolan.seaton@servicenow.com>
2018-06-22 12:33:21 -0700

    ergodow

--- keyboards/ergodox_ez/keymaps/nolan/keymap.c
+++ keyboards/ergodox_ez/keymaps/nolan/keymap.c
@@ -22,21 +22,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = KEYMAP(
 
-    KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, LGUI(KC_TAB),
-    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, MO(1),
-    CTL_T(KC_NO), KC_A, KC_S, KC_D, KC_F, KC_G,
-    KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B, LALT(KC_LGUI),
-    KC_AUDIO_MUTE, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_LALT, KC_LGUI,
-    KC_ESCAPE, KC_DELETE, KC_HOME,
-    KC_SPACE, KC_BSPACE, KC_END,
+    KC_ESCAPE,      KC_1, KC_2, KC_3, KC_4, KC_5,             KC_GRV,
+    KC_TAB,         KC_Q, KC_W, KC_E, KC_R, KC_T,             MO(1),
+    KC_LCTRL,       KC_A, KC_S, KC_D, KC_F, KC_G,
+    KC_LSHIFT,      KC_Z, KC_X, KC_C, KC_V, KC_B,             MO(1),
 
-    KC_MINUS, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQUAL,
-    KC_LBRACKET, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_RBRACKET,
-    KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE,
-    KC_BSLASH, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT,
-    KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, MO(1),
-    KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK,
-    KC_PGUP, KC_PGDOWN, KC_ENTER, KC_SPACE
+    KC_AUDIO_MUTE, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_LALT, KC_LGUI,
+
+                                                                                  KC_HOME,  KC_END,
+                                                                                            KC_SPACE,
+                                                                      KC_BSPACE, KC_DELETE, KC_LGUI,
+
+
+
+                                                                                          KC_MINUS,       KC_6, KC_7, KC_8, KC_9, KC_0,             KC_EQUAL,
+                                                                                          KC_LBRACKET,    KC_Y, KC_U, KC_I, KC_O, KC_P,             KC_RBRACKET,
+                                                                                                          KC_H, KC_J, KC_K, KC_L, KC_SCOLON,        KC_QUOTE,
+                                                                                          MO(1),          KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH,   MO(1),
+
+                                                                                          KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_BSLASH,
+
+                                                                      KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK,
+                                                                      KC_PGUP,
+                                                                      KC_PGDOWN,     KC_ENTER, KC_SPACE
 
     ),
 
@@ -52,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_F12,KC_TRANSPARENT,
     KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,
-    KC_TRANSPARENT,KC_MINUS,KC_EQUAL,KC_LBRACKET,KC_RBRACKET,KC_TRANSPARENT,KC_TRANSPARENT,
+    KC_TRANSPARENT,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,
     KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,
     KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,
     KC_TRANSPARENT,KC_TRANSPARENT,
