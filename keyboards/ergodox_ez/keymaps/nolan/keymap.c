43b6e8f4fb9facff9ee0563cfcd3c862ebc02a67
nolan.seaton <nolan.seaton@servicenow.com>
2018-06-26 11:12:01 -0700

    adding bootmagic to ergo

--- keyboards/ergodox_ez/keymaps/nolan/keymap.c
+++ keyboards/ergodox_ez/keymaps/nolan/keymap.c
@@ -23,22 +23,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = KEYMAP(
 
     KC_ESCAPE,      KC_1, KC_2, KC_3, KC_4, KC_5,             KC_GRV,
-    KC_TAB,         KC_Q, KC_W, KC_E, KC_R, KC_T,             MO(1),
+    KC_TAB,         KC_Q, KC_W, KC_E, KC_R, KC_T,             KC_LBRACKET,
     KC_LCTRL,       KC_A, KC_S, KC_D, KC_F, KC_G,
-    KC_LSHIFT,      KC_Z, KC_X, KC_C, KC_V, KC_B,             MO(1),
+    KC_LSHIFT,      KC_Z, KC_X, KC_C, KC_V, KC_B,             LSFT(KC_LBRACKET),
 
     KC_AUDIO_MUTE, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_LALT, KC_LGUI,
 
                                                                                   KC_HOME,  KC_END,
                                                                                             KC_SPACE,
-                                                                      KC_BSPACE, KC_DELETE, KC_LGUI,
+                                                                      KC_BSPACE, KC_DELETE, MO(1),
 
 
 
-                                                                                          KC_MINUS,       KC_6, KC_7, KC_8, KC_9, KC_0,             KC_EQUAL,
-                                                                                          KC_LBRACKET,    KC_Y, KC_U, KC_I, KC_O, KC_P,             KC_RBRACKET,
-                                                                                                          KC_H, KC_J, KC_K, KC_L, KC_SCOLON,        KC_QUOTE,
-                                                                                          MO(1),          KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH,   MO(1),
+                                                                                          MO(1),              KC_6, KC_7, KC_8, KC_9, KC_0,             KC_MINUS,
+                                                                                          KC_RBRACKET,        KC_Y, KC_U, KC_I, KC_O, KC_P,             KC_EQUAL,
+                                                                                                              KC_H, KC_J, KC_K, KC_L, KC_SCOLON,        KC_QUOTE,
+                                                                                          LSFT(KC_RBRACKET),  KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH,   MO(1),
 
                                                                                           KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_BSLASH,
 
