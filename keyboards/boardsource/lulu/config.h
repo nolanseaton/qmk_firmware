ee38c67892eb0fb4a0a128d3e0ba2841d6c66db7
jack <0x6A73@pm.me>
2022-08-30 01:20:05 -0600

    Add boardsource/lulu RP2040 support (#18200)
    by waffle87

--- keyboards/boardsource/lulu/config.h
+++ keyboards/boardsource/lulu/config.h
@@ -2,18 +2,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
-#include "config_common.h"
-
-#define MATRIX_ROWS 10
-#define MATRIX_COLS 6
-#define MATRIX_ROW_PINS {C6, D7, E6, B4, B5}
-
-// wiring of each half
-#define MATRIX_COL_PINS {F6, F7, B1, B3, B2, B6}
-#define USE_SERIAL
-#define SOFT_SERIAL_PIN D2
-#define SPLIT_USB_DETECT
-#define RGB_DI_PIN D3
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150
 #define DRIVER_LED_TOTAL 70
 #define RGB_MATRIX_SPLIT { 35, 35 }
